eecf7c7432f9997a3e57e1f054d6c4dd1af75386
Dmitry <30624516+Shamaev@users.noreply.github.com>
2018-08-01 17:08:41 +0300

    Lesson6

---
+++ Animal/Cat.java
@@ -0,0 +1,7 @@
+package ru.Shamaev.hw6.Animals.Animal;
+
+public class Cat extends Animals {
+    public Cat (String type, String name, int maxRun, int maxSwim, double maxJump) {
+        super(type,  name,  maxRun, maxSwim, maxJump);
+    }
+}
---
+++ Animal/Dog.java
@@ -0,0 +1,7 @@
+package ru.Shamaev.hw6.Animals.Animal;
+
+public class Dog extends Animals {
+    public Dog (String type, String name, int maxRun, int maxSwim, double maxJump) {
+        super(type,  name,  maxRun, maxSwim, maxJump);
+    }
+}
